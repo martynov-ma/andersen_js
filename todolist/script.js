6498a4c9d4731e586a613c6349c0764de277431a
Martynov Maksim <maxhome1997@gmail.com>
2017-11-11 23:08:37 +0300

    delete, check todo & styles

--- todolist/script.js
+++ todolist/script.js
@@ -1,48 +1,39 @@
-var notesCounter;
-
 window.onload = function () {
     var noteHeaderInput = document.getElementById('noteHeaderInput');
     var addNoteBtn = document.getElementById('addNoteBtn');
     var notesDiv = document.getElementById('notesDiv');
 
     addNoteBtn.onclick = addNote;
-    notesCounter = 0;
-    
 }
 
 function addNote() {
     var noteHeader = noteHeaderInput.value;
-    if (noteHeader == '') return;
+    if (!noteHeader.length) return;
     notesDiv.appendChild(newNote(noteHeader));
-
     noteHeaderInput.value = '';
-    notesCounter++;
 }
 
 function newNote(header) {
     var noteDiv = document.createElement('div');
-    noteDiv.id = 'note' + notesCounter;
-    //noteDiv.onclick = checkNote;
+    noteDiv.className = 'note';
+    noteDiv.onclick = checkNote;
 
-    var noteHeader = document.createElement('span');
-    noteHeader.innerHTML = header;
-    noteHeader.onclick = checkNote;
+    var noteHeader = document.createTextNode(header);
     noteDiv.appendChild(noteHeader);
 
-    var delNoteBtn = document.createElement('input');
-    delNoteBtn.id = 'deleteNoteBtn' + notesCounter;
-    delNoteBtn.type = 'button';
-    delNoteBtn.value = 'Удалить';
-    delNoteBtn.onclick = deleteNote;
-    noteDiv.appendChild(delNoteBtn);
+    var deleteNoteBtn = document.createElement('input');
+    deleteNoteBtn.type = 'button';
+    deleteNoteBtn.value = 'Удалить';
+    deleteNoteBtn.onclick = deleteNote;
+    noteDiv.appendChild(deleteNoteBtn);
     return noteDiv;
 }
 
 function deleteNote() {
-    alert('[delete] id = ' + this.id);
-    //notesCounter--;
+    this.parentElement.remove();
 }
 
 function checkNote() {
-    alert('[check] id = ' + this.parentElement.id);
+    if (this.className.split(' ').length == 1) this.className += ' checked';
+    else this.className = 'note';
 }
